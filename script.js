46f5654dc3c976de4081bc9ceb4c8832398c1bb2
carolina-110 <carolina-110@hotmail.com>
2019-07-03 21:41:02 -0500

    Cambios

--- script.js
+++ script.js
@@ -4,19 +4,15 @@
 let respuesta;
 
 
-// function obtener() {
-//     respuesta = document.getElementById("respuesta");
-//     console.log(respuesta);
-// }
-
-
-
-
-
 function deshabilitar() {
     op1 = document.getElementById("velocidad").value;
     op2 = document.getElementById("distancia").value;
     op3 = document.getElementById("tiempo").value;
+
+    op4 = document.getElementById("aceleracion").value;
+    op5 = document.getElementById("vi").value;
+    op6 = document.getElementById("vf").value;
+    op7 = document.getElementById("time").value;
 
 
     if (op1 !== "" && op2 !== "") {
@@ -26,7 +22,20 @@ function deshabilitar() {
     } else if (op3 !== "" && op1 !== "") {
         document.getElementById("distancia").disabled = true;
 
+    } else if (op4 !== "" && op5 !== "" && op6 !== "") {
+        document.getElementById("time").disabled = true;
     }
+    else if (op5 !== "" && op6 !== "" && op7 !== "") {
+        document.getElementById("aceleracion").disabled = true;
+    }
+    else if (op6 !== "" && op7 !== "" && op4 !== "") {
+        document.getElementById("vi").disabled = true;
+    }
+    else if (op7 !== "" && op4 !== "" && op5 !== "") {
+        document.getElementById("vf").disabled = true;
+    }
+
+
 }
 
 
@@ -36,22 +45,34 @@ function velocidadF() {
     if (op1 === "") {
         resultado = op2 / op3;
         return resultado
-        //  alert(resultado);
-        // document.getElementById('respuesta').innerHTML = velocidadF()
-
 
     } else if (op2 === "") {
         resultado = op1 * op3
-        // alert(resultado);
+
         return resultado
     } else {
         resultado = op2 / op1
-        // alert(resultado);
         return resultado
     }
 
 }
+ (vf-v1)/t
+
+
+function aceleracion(){
+    if (op4 === "") {
+        resultado = (op6 - op5)/math.pow(op7,2)
+        return resultado
+}
+
 
 function imprimirRespuesta() {
     document.getElementById('respuesta').innerHTML = velocidadF();
 }
+
+
+
+// op4 = document.getElementById("aceleracion").value;
+// op5 = document.getElementById("vi").value;
+// op6 = document.getElementById("vf").value;
+// op7 = document.getElementById("time").value;
